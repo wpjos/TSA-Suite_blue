6ca585de54efa1561d9bd7c8f7dffa400f04fee3
KeViNOne <kevinone@outlook.com>
2026-04-22 10:23:27 +0800

    feat(bianque): 初始化套件、引擎、算子模块
    - 新增 bianque 包的初始化文件，定义版本号和基础描述
    - 添加执行引擎模块初始化文件，导入 bianque 并同步版本信息
    - 增加算子模块初始化文件，设置模块注释及空的导出列表

---
+++ src/bianque/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Full-featured Time Series Analysis Suite
+
+一个面向时间序列分析的全功能扁鹊套件
+"""
+
+__version__ = '0.2.1'
---
+++ src/bianque/engine/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+BianQue-Suite的执行引擎
+"""
+
+import bianque
+
+__version__ = bianque.__version__
+
+__all__ = []
